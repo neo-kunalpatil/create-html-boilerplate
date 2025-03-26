8bcb7b398b5ed264a9c4e6a35acf61d7b090f43c
neo-kunalpatil <kunalpatil3435@gmail.com>
2025-03-26 22:46:46 +0530

    kunal

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -9,7 +9,7 @@ module.exports = {
       },
     ],
     'declaration-no-important': true,
-    'length-zero-no-unit': true,
+    'length-zero-no-unit there are one kunal ': true,
     'selector-class-pattern':
       '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
